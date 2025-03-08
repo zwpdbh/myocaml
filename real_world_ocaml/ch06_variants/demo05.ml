3ea22aa1e90b171204a6ca6213602d134304c6b0
zwpdbh <hyperion_z@outlook.com>
2025-03-08 09:10:35 +0800

    > ch07 error handling

--- real_world_ocaml/ch06_variants/demo05.ml
+++ real_world_ocaml/ch06_variants/demo05.ml
@@ -35,33 +35,62 @@ let rec eval expr base_eval =
   match expr with
   | Base base -> base_eval base
   | Const bool -> bool
-  | And exprs -> List.for_all exprs ~f:eval'
-  | Or exprs -> List.exists exprs ~f:eval'
+  | And exprs -> List.for_all (fun each_exp -> eval' each_exp) exprs
+  | Or exprs -> List.exists (fun each_exp -> eval' each_exp) exprs
   | Not expr -> not (eval' expr)
 
 (* Another useful operation on expressions is simplification, which is the process of taking a boolean expression and reducing it to an equivalent one that is smaller. *)
-let and_ l =
+(* let and_ l =
   if List.exists l ~f:(function Const false -> true | _ -> false) then Const false
   else
     match List.filter l ~f:(function Const true -> false | _ -> true) with
     | [] -> Const true
     | [ x ] -> x
-    | l -> And l
+    | l -> And l *)
 
-let or_ l =
+let and_ l =
+  (* Step 1: Check if any element is Const false *)
+  let has_const_false = List.exists (fun e -> match e with Const false -> true | _ -> false) l in
+  if has_const_false then
+    Const false (* Short-circuit: if any element is false, the result is false *)
+  else
+    (* Step 2: Remove all Const true elements *)
+    let non_true_elements =
+      List.filter (fun e -> match e with Const true -> false | _ -> true) l
+    in
+    match non_true_elements with
+    | [] -> Const true (* If all elements were Const true, the result is true *)
+    | [ x ] -> x (* If only one element remains, return it *)
+    | _ -> And non_true_elements (* Otherwise, create a new And expression *)
+
+(* let or_ l =
   if List.exists l ~f:(function Const true -> true | _ -> false) then Const true
   else
     match List.filter l ~f:(function Const false -> false | _ -> true) with
     | [] -> Const false
     | [ x ] -> x
-    | l -> Or l
+    | l -> Or l *)
+
+let or_ l =
+  (* Step 1: Check if any element is Const true *)
+  let has_const_true = List.exists (fun e -> match e with Const true -> true | _ -> false) l in
+  if has_const_true then Const true (* Short-circuit: if any element is true, the result is true *)
+  else
+    (* Step 2: Remove all Const false elements *)
+    let non_false_elements =
+      List.filter (fun e -> match e with Const false -> false | _ -> true) l
+    in
+    match non_false_elements with
+    | [] -> Const false (* If all elements were Const false, the result is false *)
+    | [ x ] -> x (* If only one element remains, return it *)
+    | _ -> Or non_false_elements (* Otherwise, create a new Or expression *)
 
 let not_ = function Const b -> Const (not b) | (Base _ | And _ | Or _ | Not _) as e -> Not e
 
 let rec simplify = function
   | (Base _ | Const _) as x -> x
-  | And l -> and_ (List.map ~f:simplify l)
-  | Or l -> or_ (List.map ~f:simplify l)
+  | And l -> and_ (List.map (fun e -> simplify e) l)
+  | Or l -> or_ (List.map (fun e -> simplify e) l)
   | Not e -> not_ (simplify e)
 
 let _ = simplify (Not (And [ Or [ Base "it's snowing"; Const true ]; Base "it's raining" ]))
--- real_world_ocaml/ch06_variants/demo06.ml
+++ real_world_ocaml/ch06_variants/demo06.ml
@@ -1,2 +1,40 @@
 (* https://dev.realworldocaml.org/variants.html#variants-and-recursive-data-structures *)
 (* Polymorphic variants *)
+(* In practise, it let us share tags between two different variant types *)
+open Base
+open Stdio
+
+type basic_color = Black | Red | Green | Yellow | Blue | Magenta | Cyan | White
+type weight = Regular | Bold
+
+type color =
+  | Basic of basic_color * weight (* basic colors, regular and bold *)
+  | RGB of int * int * int (* 6x6x6 color cube *)
+  | Gray of int (* 24 grayscale levels *)
+
+let basic_color_to_int = function
+  | `Black -> 0
+  | `Red -> 1
+  | `Green -> 2
+  | `Yellow -> 3
+  | `Blue -> 4
+  | `Magenta -> 5
+  | `Cyan -> 6
+  | `White -> 7
+
+let color_to_int = function
+  | `Basic (basic_color, weight) ->
+      let base = match weight with Bold -> 8 | Regular -> 0 in
+      base + basic_color_to_int basic_color
+  | `RGB (r, g, b) -> 16 + b + (g * 6) + (r * 36)
+  | `Gray i -> 232 + i
+
+type extended_color =
+  | Basic of basic_color * weight (* basic colors, regular and bold *)
+  | RGB of int * int * int (* 6x6x6 color space *)
+  | Gray of int (* 24 grayscale levels *)
+  | RGBA of int * int * int * int (* 6x6x6x6 color space *)
+
+let extended_color_to_int = function
+  | `RGBA (r, g, b, a) -> 256 + a + (b * 6) + (g * 36) + (r * 216)
+  | (`Basic _ | `RGB _ | `Gray _) as color -> color_to_int color
