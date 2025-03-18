865064b1b3a6cdb923855601d61798a7ee917b6c
zwpdbh <hyperion_z@outlook.com>
2025-03-18 22:38:11 +0800

    > Ivars and Upon

--- real_world_ocaml/ch16_concurrent_programming/simple_async.ml
+++ real_world_ocaml/ch16_concurrent_programming/simple_async.ml
@@ -2,8 +2,29 @@ open Core
 open Async
 open Reader
 
-let process_file filename uppercase =
+(* Deferred.bind d ~f causes f to be called after the value of d has been determined. *)
+(* Together, bind and return form a design pattern in functional programming known as a monad. *)
+let process_file_v1 filename uppercase =
+  Deferred.bind (Reader.file_contents filename) ~f:(fun text ->
+      let processed_text = if uppercase then String.uppercase text else String.lowercase text in
+      print_endline processed_text;
+      shutdown 0;
+      return ())
+
+(*  Infix operator >>= is a shorthand for Deferred.bind. *)
+let process_file_v2 filename uppercase =
   Reader.file_contents filename >>= fun text ->
+  let processed_text = if uppercase then String.uppercase text else String.lowercase text in
+  print_endline processed_text;
+  shutdown 0;
+  (* return, it is a function provided by Async that takes an ordinary value and wrap it up in a deferred. *)
+  return ()
+
+(* #require "ppx_let";; 
+  Opening Async also implicitly opens Deferred.Let_syntax
+*)
+let process_file_v3 filename uppercase =
+  let%bind text = Reader.file_contents filename in
   let processed_text = if uppercase then String.uppercase text else String.lowercase text in
   print_endline processed_text;
   shutdown 0;
@@ -20,7 +41,7 @@ let command =
   let params =
     let%map_open.Command file_name = flag "-f" (required string) ~doc:"file The filename to read"
     and uppercase = flag "-u" no_arg ~doc:"Convert to uppercase (default is lowercase)" in
-    fun () -> process_file file_name uppercase
+    fun () -> process_file_v3 file_name uppercase
   in
   let readme () =
     "This command demonstrates asynchronous file reading in OCaml. It reads the contents of a \
