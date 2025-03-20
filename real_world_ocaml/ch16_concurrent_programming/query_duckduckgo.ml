fca4963a8cb7bcc5ec6dc961b1292b3d1076d331
zwpdbh <hyperion_z@outlook.com>
2025-03-20 20:30:28 +0800

    > update cohttp-async

--- real_world_ocaml/ch16_concurrent_programming/query_duckduckgo.ml
+++ real_world_ocaml/ch16_concurrent_programming/query_duckduckgo.ml
@@ -2,5 +2,28 @@ open Core
 open Async
 
 (*a small command-line utility for querying DuckDuckGo to extract definitions for a collection of terms. 
-
 It relies on textwrap, uri, yojson, cohttp*)
+
+(* Generate a DuckDuckGo search URI from a query string *)
+let query_uri query =
+  let base_uri = Uri.of_string "http://api.duckduckgo.com/?format=json" in
+  Uri.add_query_param base_uri ("q", [ query ])
+
+(* Extract the "Definition" or "Abstract" field from the DuckDuckGo
+   results *)
+let get_definition_fromn_json json =
+  match Yojson.Safe.from_string json with
+  | `Assoc kv_list -> (
+      let find key =
+        match List.Assoc.find ~equal:String.equal kv_list key with
+        | None | Some (`String "") -> None
+        | Some s -> Some (Yojson.Safe.to_string s)
+      in
+      match find "Abstract" with Some _ as x -> x | None -> find "Definition")
+  | _ -> None
+
+(* Execute the DuckDuckGo search *)
+let get_definition word =
+  let%bind _, body = Cohttp_async.Client.get (query_uri word) in
+  let%map string = Cohttp_async.Body.to_string body in
+  (word, get_definition_fromn_json string)
