03475cce15aa5d52975f57fd40dc9909e99f9b31
zwpdbh <hyperion_z@outlook.com>
2025-03-21 23:29:33 +0800

    > ch17 test

--- real_world_ocaml/ch16_concurrent_programming/ch16.ml
+++ real_world_ocaml/ch16_concurrent_programming/ch16.ml
@@ -1,6 +1,26 @@
 open Core
 open Async
 
+(*  Deferred.both, which lets you wait until two deferreds of different types have returned, returning both values as a tuple *)
+let string_and_float =
+  Deferred.both
+    (let%map () = after (sec 0.5) in
+     "A")
+    (let%map () = after (sec 0.25) in
+     32.33)
+
+(* Timeouts, Cancellation, and Choices *)
+let any_one_of_them_is_determined =
+  Deferred.any
+    [
+      (let%map () = after (sec 0.5) in
+       "half a second");
+      (let%map () = after (sec 1.0) in
+       "one second");
+      (let%map () = after (sec 4.0) in
+       "four seconds");
+    ]
+
 let command =
   Command.group ~summary:"Ch016: Concurrent Programming"
     [
--- real_world_ocaml/ch16_concurrent_programming/query_duckduckgo.ml
+++ real_world_ocaml/ch16_concurrent_programming/query_duckduckgo.ml
@@ -11,7 +11,7 @@ let query_uri query =
 
 (* Extract the "Definition" or "Abstract" field from the DuckDuckGo
    results *)
-let get_definition_fromn_json json =
+let get_definition_from_json json =
   match Yojson.Safe.from_string json with
   | `Assoc kv_list -> (
       let find key =
@@ -23,33 +23,65 @@ let get_definition_fromn_json json =
   | _ -> None
 
 (* Execute the DuckDuckGo search *)
-let get_definition word =
-  let%bind _, body = Cohttp_async.Client.get (query_uri word) in
-  let%map string = Cohttp_async.Body.to_string body in
-  (word, get_definition_fromn_json string)
 
-let get_definition word =
-  let%bind _, body = Cohttp_async.Client.get (query_uri word) in
-  let%map string = Cohttp_async.Body.to_string body in
-  let the_definition = get_definition_fromn_json string in
-  (word, the_definition)
+let get_definition ~interrupt word =
+  match%map
+    try_with (fun () ->
+        let%bind _, body = Cohttp_async.Client.get ~interrupt (query_uri word) in
+        let%map string = Cohttp_async.Body.to_string body in
+        let the_definition = get_definition_from_json string in
+        (word, the_definition))
+  with
+  | Ok (word, definition) -> (word, Ok definition)
+  | Error _ -> (word, Error "unexpected failure")
+
+let get_definition_with_timeout_v1 word =
+  let timeout_duration = sec 5.0 in
+  match%map get_definition ~interrupt:(after timeout_duration) word with
+  | word, Error _ -> (word, Error "Unexpected failure")
+  | word, (Ok _ as x) -> (word, x)
+
+(* Improved from v1 that now we could distinguish between exception error and timeout error *)
+(* Use Ivar is important:
+In essence, the Ivar here acts as a one-time, asynchronous flag. It's a way to send a signal from one part of the asynchronous computation (the timeout logic) to another (the get_definition function) without requiring synchronous communication or polling. This pattern allows for efficient, non-blocking cancellation of asynchronous operations in OCaml's Async library.
+*)
+let get_definition_with_timeout_v2 word =
+  let timeout_duration = sec 5.0 in
+  let interrupt = Ivar.create () in
+  choose
+    [
+      choice (after timeout_duration) (fun () ->
+          (* when timeout, interrupt is filled, become determined *)
+          Ivar.fill interrupt ();
+          (word, Error "Timed out"));
+      choice
+        (* If the timeout occurs first, the interrupt Ivar is filled, which then allows the interrupt deferred in get_definition to complete, triggering an early return with an error. *)
+        (get_definition ~interrupt:(Ivar.read interrupt) word)
+        (fun (word, result) ->
+          let result' =
+            match result with Ok _ as x -> x | Error _ -> Error "Unexpected failure"
+          in
+          (word, result'));
+    ]
 
 (* Print out a word/definition pair *)
 let print_result (word, definition) =
-  printf "%s\n %s\n \n %s\n \n" word
+  printf "%s\n%s\n\n%s\n\n" word
     (String.init (String.length word) ~f:(fun _ -> '-'))
     (match definition with
-    | None -> "No definition found"
-    | Some def -> String.concat ~sep:"\n" (Wrapper.wrap (Wrapper.make 70) def))
+    | Error s -> "DuckDuckGo query failed: " ^ s
+    | Ok None -> "No definition found"
+    | Ok (Some def) -> String.concat ~sep:"\n" (Wrapper.wrap (Wrapper.make 70) def))
 
 (* Run many searches in parallel, printing out the results after
    they're all done. *)
 let search_and_print words =
-  let%map results = Deferred.all (List.map words ~f:get_definition) in
+  let%map results = Deferred.all (List.map words ~f:get_definition_with_timeout_v1) in
   List.iter results ~f:print_result
 
 let search_and_printv2 words =
-  Deferred.all_unit (List.map words ~f:(fun word -> get_definition word >>| print_result))
+  Deferred.all_unit
+    (List.map words ~f:(fun word -> get_definition_with_timeout_v1 word >>| print_result))
 
 (* dune exec real_world_ocaml/ch16_concurrent_programming/ch16.exe demo04 -- -search "ocaml functional","programming" *)
 let command =
